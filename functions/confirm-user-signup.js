03a139554fbd22f8486f42297e9c63d37d608053
pk <pipalpurus@gmail.com>
2021-06-18 13:27:53 +0545

    issue fixes

--- functions/confirm-user-signup.js
+++ functions/confirm-user-signup.js
@@ -1,30 +1,38 @@
 const DynamoDB = require('aws-sdk/clients/dynamodb');
-const DocumentClint = new DynamoDB.DocumentClient();
+const DocumentClient = new DynamoDB.DocumentClient();
 const Chance = require('chance');
 const chance = new Chance();
 
-const { USER_TABLE } = process.env;
+const { USERS_TABLE } = process.env;
 
 module.exports.handler = async (event) => {
   if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
     const name = event.request.userAttributes['name'];
-    const suffix = chance.string({ length: 8, alpha: 'upper', numeric: true });
+    const suffix = chance.string({
+      length: 8,
+      casing: 'upper',
+      alpha: true,
+      numeric: true
+    });
     const screenName = `${name.replace(/[^a-zA-Z0-9]/g, '')}${suffix}`;
-
     const user = {
       id: event.userName,
-      name: screenName,
-      createdA: new Date().toJSON(),
+      name,
+      screenName,
+      createdAt: new Date().toJSON(),
       followersCount: 0,
       followingCount: 0,
       tweetsCount: 0,
-      likeCount: 0
+      likesCounts: 0
     };
-    await DocumentClint.put({
-      TableName: USER_TABLE,
+
+    await DocumentClient.put({
+      TableName: USERS_TABLE,
       Item: user,
-      ConditionExpression: 'attribute_not_exits(id)'
+      ConditionExpression: 'attribute_not_exists(id)'
     }).promise();
+
+    return event;
   } else {
     return event;
   }
